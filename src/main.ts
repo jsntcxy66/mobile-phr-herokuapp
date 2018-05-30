f15ae1d50aef3da01689eebae83839ff2d5df96a
Xinyi Chen <xic98@pitt.edu>
2018-05-30 16:37:23 -0400

    console log2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as express from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(__dirname);
+  console.log(path.join(__dirname, 'dist'));
   app.use(express.static(path.join(__dirname, 'dist')));
   await app.listen(process.env.PORT || 3000);
 }
